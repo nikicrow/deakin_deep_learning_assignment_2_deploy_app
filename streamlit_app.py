9981f71f00dcfc0ae00854ebfd26702963a568a5
ZM\niki.crow <niki.crow@zip.co>
2022-09-04 19:21:48 +1000

    test image uploader

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 # Add app title
 st.title('Is this Recyclable?')
+st.write("Upload any image and we will tell you if it is recyclable or not")
 
+uploaded_file = st.file_uploader("Please upload an image using this image uploader")
+
+
+#loaded_model = tf.keras.models.load_model('drive/MyDrive/Colab Notebooks/models/model_2_augmented.h5')
