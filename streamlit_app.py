2f58b8f8cdb743632ea90025ba9aa9f83afd762d
ZM\niki.crow <niki.crow@zip.co>
2022-09-05 20:25:51 +1000

    add dotenv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+from dotenv import load_dotenv
+import os
 
 # Add app title
 st.title('Is this Recyclable?')
@@ -12,6 +14,26 @@ uploaded_file = st.file_uploader("Please upload an image using this image upload
 
 #loaded_model = tf.keras.models.load_model('drive/MyDrive/Colab Notebooks/models/model_2_augmented.h5')
 def is_recyclable(uploaded_file):
+
+    my_env_file = 'KEYS.env'
+    env_path=os.path.join('env_var_folder', my_env_file)
+    load_dotenv(env_path)
+
+    AWS_KEY=os.getenv('AWS_KEY')
+    AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
+    BUCKET=os.getenv('BUCKET')
+
+    import boto3
+    s3_resource = boto3.resource('s3', 
+                                aws_access_key_id = AWS_KEY,
+                                aws_secret_access_key = AWS_SECRET_KEY, 
+                                region_name='ap-southeast-2')
+
+    bucket = BUCKET
+    key = 'models/model_2_augmented.h5'
+
+    loaded_model = s3_resource.Bucket(bucket).Object(key).get()['Body'].read()
+
     return True
 
 # If we get a picture uploaded, lets do stuff to it!
